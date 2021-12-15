4be934671aa710f7d1486e9450eb2c4617548c33
CocytusElias <elias@elias.ink>
2021-12-15 13:19:26 +0800

    feat: 增加git指令以及修改文件CRLF为LF

--- src/commands/init/ts-package/git.ts
+++ src/commands/init/ts-package/git.ts
@@ -1,9 +1,16 @@
 import { BasicAnswersType } from "./index";
+import { execSync } from 'child_process';
 
-export function create(basicAnswers: BasicAnswersType) {
-  // TODO: git
-  //    初始化git
-  //    建立git分支
-  //    建立存储库
-  //    连接存储库
+export async function create(basicAnswers: BasicAnswersType) {
+  // FIXME: 修复执行失败的问题
+  const gitCommandArray : string[] = [
+    "git init",
+    "git add .",
+    'git commit -m "first commit"',
+    "git branch dev",
+    `git remote add origin ${basicAnswers.repository}`
+  ]
+  for (let command of gitCommandArray){
+    await execSync(command, { cwd: basicAnswers.initPath })
+  }
 }
