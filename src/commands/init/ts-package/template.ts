06ed0767b24ad07fef553ac82edd485c07809823
CocytusElias <elias@elias.ink>
2022-01-10 18:09:56 +0800

    fix: 完善模版建立及模版内容

--- src/commands/init/ts-package/template.ts
+++ src/commands/init/ts-package/template.ts
@@ -3,31 +3,40 @@ import * as fs from "fs";
 import * as path from "path";
 import * as LocalStorage from '../../../utils/local-storage';
 
-const CopyFileArray = [
-  "package.json",
-  "README.md",
-  ".gitignore",
-]
-
-export function create(basicAnswers: AnswersType) {
-  // let localStorage = LocalStorage.read()
-  // if (!basicAnswers.repository){
-    // basicAnswers.repository = `${localStorage.author.url}/${basicAnswers.name}`
-  // }
-  for (let fileName of CopyFileArray ){
-    // let file_content = fs.readFileSync(path.resolve( __dirname, "..", "..", "..", "template", "ts-package", fileName), "utf-8")
-    // file_content = file_content.replace(/\${project\.name}/g, basicAnswers.name)
-    // file_content = file_content.replace(/\${project\.description}/g, basicAnswers.description)
-    // file_content = file_content.replace(/\${project\.repository}/g, basicAnswers.repository)
-    // file_content = file_content.replace(/\${project\.main}/g, basicAnswers.main)
-    // file_content = file_content.replace(/\${project\.types}/g, basicAnswers.types)
-    // file_content = file_content.replace(/\${project\.srcDir}/g, basicAnswers.srcDir)
-    // file_content = file_content.replace(/\${project\.outDir}/g, basicAnswers.outDir)
-    // file_content = file_content.replace(/\${project\.name-badge}/g, basicAnswers.name.replace(/\-/g,"--"))
-    // file_content = file_content.replace(/\${author\.name}/g, localStorage.author.name)
-    // file_content = file_content.replace(/\${author\.email}/g, localStorage.author.email)
-    // file_content = file_content.replace(/\${author\.url}/g, localStorage.author.url)
-    // fs.writeFileSync(path.join(basicAnswers.initPath, fileName),file_content, "utf-8")
+export async function create(basicAnswers: AnswersType) {
+  let localStorage = LocalStorage.read()
+  const CopyFileArray = [
+    { fileName: ".gitignore", params: [ "outDir" ] },
+    { fileName: "package.json", params: [ "pkgName", "description", "mainName", "tsName", "outDir", "srcDir", "sshRepositoryUrl", "authorName", "authorEmail", "authorUrl", "repositoryWebUrl" ] },
+    { fileName: "README.md", params: [ "pkgName" , "description", "authorName", "authorUrl"] },
+    { fileName: "tsconfig.json", params: [ "outDir" ] },
+  ]
+  const ReplaceData : {[index: string] : string} = {
+    "pkgName": basicAnswers.pkgName,
+    "description": basicAnswers.description,
+    "outDir": basicAnswers.outDir,
+    "srcDir": basicAnswers.srcDir,
+    "mainName": `${basicAnswers.mainName}.js`,
+    "tsName": `${basicAnswers.tsName}.ts`,
+    "httpRepositoryUrl": basicAnswers.httpRepositoryUrl,
+    "repositoryWebUrl": basicAnswers.repositoryWebUrl,
+    "authorName": localStorage.author.name,
+    "authorUrl": localStorage.author.url,
+    "authorEmail": localStorage.author.email,
   }
 
+  for (let fileInfo of CopyFileArray ){
+    let file_content = fs.readFileSync(path.resolve( __dirname, "..", "..", "..", "template", "ts-package", fileInfo.fileName), "utf-8")
+
+    for (let param of fileInfo.params){
+      file_content = file_content.replace(new RegExp(`\\\${${param}}`,"g"), ReplaceData[param])
+      console.log("--------")
+      console.log(file_content)
+      console.log(new RegExp(`\\\${${param}}`,"g"))
+      console.log(ReplaceData[param])
+      console.log("--------")
+    }
+
+    await fs.writeFileSync(path.join(basicAnswers.pkgPath, fileInfo.fileName), file_content, "utf-8")
+  }
 }
